ad98f9a2cc2ae4e09d49a3e6f1fd6d2c54b32572
LEMSantos <lucas_m-santos@hotmail.com>
2020-11-19 12:51:57 -0300

    Add - waitress como server principal da aplicação

--- botlander/__main__.py
+++ botlander/__main__.py
@@ -1,4 +1,9 @@
+from botlander.api import app
 from mongoengine import connect
+from paste.translogger import TransLogger
+import waitress
+import env
+
 connect(
     env.DATABASE_NAME,
     host=env.DATABASE_HOST,
@@ -6,3 +11,11 @@ connect(
     password=env.DATABASE_PASSWORD,
     authentication_source=env.DATABASE_AUTHENTICATION_SOURCE,
 )
+
+if __name__ == '__main__':
+    waitress.serve(
+        TransLogger(app, setup_console_handler=False),
+        host=env.WAITRESS_HOST,
+        port=env.WAITRESS_PORT,
+        threads=env.WAITRESS_THREADS,
+    )
