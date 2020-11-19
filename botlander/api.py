2b5d60fb8d6e71cf932bd3247c955c360e5d8acf
LEMSantos <lucas_m-santos@hotmail.com>
2020-11-19 12:52:42 -0300

    Add - configurações iniciais da api

--- botlander/api.py
+++ botlander/api.py
@@ -1,9 +1,28 @@
 from flask import Flask
-from flask_restful import Resource, Api
+from flask_cors import CORS
+from flask_restful import Api
+from botlander.resources import (
+    UserResource,
+    UserResourceList,
+    LoginResource,
+    RefreshResource,
+    FreshLoginResource,
+    BotResource,
+    BotResourceList,
+    BotImageResource,
+)
+from flask_jwt_extended import JWTManager
+import env
 
 
 app = Flask(__name__)
 api = Api(app)
+jwt = JWTManager(app)
+CORS(app)
+
+app.config['JWT_SECRET_KEY'] = env.JWT_SECRET_KEY
+app.config['JWT_REFRESH_TOKEN_EXPIRES'] = env.JWT_REFRESH_TOKEN_EXPIRES
+app.config['JWT_ACCESS_TOKEN_EXPIRES'] = env.JWT_ACCESS_TOKEN_EXPIRES
 
 class HelloWorld(Resource):
     def get(self):
