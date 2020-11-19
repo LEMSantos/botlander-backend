2672b5f7778212a06a1b0615cabac8bf39b9fd62
LEMSantos <lucas_m-santos@hotmail.com>
2020-11-19 12:54:22 -0300

    Add - model para a collection de usuários

---
+++ botlander/database/models/user.py
@@ -0,0 +1,16 @@
+from mongoengine import Document
+from mongoengine import (
+    StringField,
+    EmailField,
+)
+
+
+class User(Document):
+
+    __collection__ = 'users'
+
+    name = StringField(max_length=100, required=True)
+    lastname = StringField(max_length=100, required=True)
+    username = StringField(max_length=100, required=True, unique=True)
+    email = EmailField(required=True, unique=True)
+    password = StringField(required=True)
