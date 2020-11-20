1e490e8068af13320faa0a7ad3d236546b7fdaff
LEMSantos <lucas_m-santos@hotmail.com>
2020-11-19 22:25:43 -0300

    Fix - problema ao gerar o api_token

--- botlander/database/models/bot.py
+++ botlander/database/models/bot.py
@@ -20,7 +20,7 @@ class Bot(Document):
     )
     description = StringField(max_length=300)
     image = ImageField()
-    api_token = StringField(default=uuid.uuid1().hex, unique=True)
+    api_token = StringField(default=lambda: str(uuid.uuid4()), unique=True)
     telegram_token = StringField()
     user_id = ReferenceField(
         User,
