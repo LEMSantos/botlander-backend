b4b66542a3a70d9b6d3facd5b6a30ca4f967a1d9
LEMSantos <lucas_m-santos@hotmail.com>
2020-11-25 18:01:58 -0300

    Add - exportação de novas models

--- botlander/database/models/__init__.py
+++ botlander/database/models/__init__.py
@@ -1,7 +1,13 @@
 from botlander.database.models.user import User
 from botlander.database.models.bot import Bot
+from botlander.database.models.statement import Statement
+from botlander.database.models.action import Action
+from botlander.database.models.validation import Validation
 
 __all__ = (
     'User',
     'Bot',
+    'Statement',
+    'Action',
+    'Validation',
 )
